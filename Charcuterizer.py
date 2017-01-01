fb65709a1380e836a5f56914ffdea9f30e206535
Hugh Dickinson <hugh.dickinson@fysik.su.se>
2017-01-01 18:11:53 +0000

    Added Scheduling class

--- Charcuterizer.py
+++ Charcuterizer.py
@@ -14,11 +14,21 @@ from HumidifierControllerClass import HumidifierController
 from DehumidifierControllerClass import DehumidifierController
 
 # The following line imports the code for the class (blueprint) which defines the way
-# that objects of type HumidifierController behave, from a file called
-# HumidifierControllerClass.py
+# that objects of type HeaterController behave, from a file called
+# HeaterControllerClass.py
 from HeaterControllerClass import HeaterController
 
+# The following line imports the code for the class (blueprint) which defines the way
+# that objects of type schedule behave, from a file called
+# ScheduleClass.py
+from ScheduleClass import Schedule
+
 import time
+import datetime
+
+# create and initialize a schedule
+schedule = Schedule([3, 4, 5], [(18, 20),(25, 27),(18, 25)], [(60, 70),(80, 90),(65, 85)])
+schedule.printVariables()
 
 # create an object of type HumTemSensor, the first argument is the GPIO pin
 # number dusing the BROADCOM numbering scheme
---
+++ ScheduleClass.py
@@ -0,0 +1,49 @@
+import datetime
+
+class Schedule :
+
+    transitionTimes = []
+    temperatureRanges = []
+    humidityRanges = []
+
+    currentStage = 0
+
+    def __init__(self, stageDurationsArg, temperatureRangesArg, humidityRangesArg) :
+        self.createTransitionTimes(stageDurationsArg)
+        self.humidityRanges = humidityRangesArg
+        self.temperatureRanges = temperatureRangesArg
+
+    def update(self) :
+        updateRequired = self.isUpdateRequired()
+        if updateRequired is not None and updateRequired :
+            self.currentStage += 1
+
+    def isUpdateRequired(self) :
+        if not self.isComplete(self.currentStage) :
+            return datetime.date.today() > self.transitionTimes[currentStage]
+        else :
+            return None
+
+    def createTransitionTimes(self, stageDurations) :
+        for duration in stageDurations :
+            if len(transitionTimes) == 0 :
+                self.transitionTimes.append(datetime.date.today() + datetime.timedelta(days = duration))
+            else :
+                self.transitionTimes.append(self.transitionTimes[-1] + datetime.timedelta(days = duration))
+
+    def printVariables(self) :
+        print("Schedule:")
+        print("Transition times:\n{}".format(self.transitionTimes))
+        print("Temperature Ranges\n{}".format(self.temperatureRanges))
+        print("Humidity Ranges:\n{}".format(self.humidityRanges))
+
+    def isComplete(self) :
+        return self.currentStage == len(self.transitionTimes) - 1
+
+    def getHumidityRange(self) :
+        self.update()
+        return self.humidityRanges[self.currentStage]
+
+    def getTemperatureRange(self) :
+        self.update()
+        return self.temperatureRanges[self.currentStage]
