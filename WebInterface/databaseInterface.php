98d2bf601d1d50a07fa3f22f2ce0d08145fbc91c
Hugh Dickinson <hdickins@umn.edu>
2017-01-20 21:28:37 -0600

    Added recipe summary

--- WebInterface/databaseInterface.php
+++ WebInterface/databaseInterface.php
@@ -1,4 +1,22 @@
 <?php
+
+$inputNames = ['durationDays', 'minTempC', 'maxTempC', 'minHumPc', 'maxHumPc'];
+$inputDescriptions = [ 'Stage', 'Duration (days)',	'Min. Temp. (C)',	'Max. Temp. (C)',	'Min. Hum. (%)',	'Max. Hum. (%)' ];
 $databaseQuery  = $_POST;
-echo 'posted'.var_dump($databaseQuery);
- ?>
+$returnString = '<h1>RECIPE SUMMARY:</h1>'.
+'<h3>NAME: '.$_POST['recipeName'].'</h3>'.
+'<h3>STAGES: '.count($_POST['durationDays']).'</h3><table><tr>';
+for ($iInput = 0; $iInput < count($inputDescriptions); $iInput++){
+  $returnString .= '<td>'.$inputDescriptions[$iInput].'<td>';
+}
+$returnString .= '</tr>';
+for ($iStage = 0; $iStage < count($_POST['durationDays']); $iStage++){
+  $returnString .= '<tr><td>'.$iStage.'</td>';
+  for ($iInput = 0; $iInput < count($inputNames); $iInput++){
+    $returnString .= '<td>'.$_POST[$inputNames[$iInput]][$iStage].'<td>';
+  }
+  $returnString .= '</tr>';
+}
+
+echo $returnString.'</table>';
+?>
